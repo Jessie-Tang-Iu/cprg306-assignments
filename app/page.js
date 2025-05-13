90a846ac701ea09b7afc8847e01b391cc8fbc63a
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 08:59:55 -0600

    Update page.js
    adding link to home page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 
 export default function Home() {
@@ -5,6 +6,11 @@ export default function Home() {
     <main>
       <h1 className="text-3xl">Hello World!</h1>
       <p>Test123</p>
+
+      <h2>Course Example Links</h2>
+      <ul>
+        <li> <Link href="./week-2/">Week 2 - Intro to React</Link> </li>
+      </ul>
     </main>
   );
 }
