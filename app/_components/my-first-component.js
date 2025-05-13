6045f87d6342ebaf5731fe21aea7f66fa4b1f03d
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 09:36:44 -0600

    Update my-first-component.js
    adding the className

--- app/_components/my-first-component.js
+++ app/_components/my-first-component.js
@@ -5,7 +5,7 @@ export default function MyFirstComponent() {
     return (
         <div>
             <h3>My First Component</h3>
-            <Link href="https://www.sait.ca">SAIT Website</Link>
+            <Link href="https://www.sait.ca" className="text-cyan-600 underline hover:text-cyan-300">SAIT Website</Link>
         </div>
     );
 }
