3befb9e3ba15f6501da189e0bb4b4cc84c539da8
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 10:51:13 -0600

    Create page, item and itemList for asm 3

---
+++ app/week-3/item.js
@@ -0,0 +1,17 @@
+
+
+export default function Item({props}) {
+
+    let item = { 
+        name: props.name, 
+        quantity: props.quantity, 
+        category: props.category,
+    };
+
+    return (
+        <p className="px-4 py-2 m-8 bg-slate-800">
+            <p className="text-lg font-bold">{item.name}</p>
+            <p>Buy {item.quantity} in {item.category}</p>
+        </p>
+    );
+}
