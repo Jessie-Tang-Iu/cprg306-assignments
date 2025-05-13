7d846389f90d41d02427b6261bdaabb253e2f37e
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 10:10:02 -0600

    Update page.js
    styling my home page

--- app/page.js
+++ app/page.js
@@ -4,11 +4,9 @@ import Link from "next/link";
 export default function Home() {
     return (
     <main>
-      {
-        // test
-      }
-      <h1 className="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
-      <h2>Course Assignment Links</h2>
+      <h1 className="text-3xl"><strong>CPRG 306: Web Development 2 - Assignments</strong></h1>
+      <br />
+      <h2>Course Assignment Links : </h2>
       <ul>
         <li> <Link href="./week-2/" className="text-cyan-600 underline hover:text-cyan-300">Week 2 - Link To My Week 2 Assignment</Link> </li>
       </ul>
