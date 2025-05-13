b11b1ee02acdf630544cf51718e7dd38287c01e3
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 09:24:38 -0600

    Update page.js
    change the link style

--- app/page.js
+++ app/page.js
@@ -2,14 +2,24 @@ import Link from "next/link";
 
 
 export default function Home() {
-  return (
+
+  // this is a single line comment
+  /*
+  this is a 
+  multi-line comment
+   */
+
+    return (
     <main>
+      {
+        // test
+      }
       <h1 className="text-3xl">Hello World!</h1>
       <p>Test123</p>
 
       <h2>Course Example Links</h2>
       <ul>
-        <li> <Link href="./week-2/">Week 2 - Intro to React</Link> </li>
+        <li> <Link href="./week-2/" className="text-cyan-600 underline hover:text-cyan-300">Week 2 - Intro to React</Link> </li>
       </ul>
     </main>
   );
