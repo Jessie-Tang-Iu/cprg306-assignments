cffae548a237d551f5e4018dcd283c983cba6603
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 09:36:55 -0600

    Update page.js
    adding component to week 2 page

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import MyFirstComponent from "../_components/my-first-component";
 
 
 export default function WeekTwo() {
@@ -6,7 +7,7 @@ export default function WeekTwo() {
         <main>
             <h1>Second Page</h1>
             <p>This is the second page of my React app!</p>
-
+            <MyFirstComponent />
             <Link href="/" className="text-cyan-600 underline hover:text-cyan-300">Home</Link>
         </main>
     );
