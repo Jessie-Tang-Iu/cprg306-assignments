51ba2f6d7ba31e3f2a4b795c5ca3205c5c0cb1cd
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 08:50:54 -0600

    Update page.js
    change in home page

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3x1">Hello World!</h1>
+      <h1 className="text-3xl">Hello World!</h1>
       <p>Test123</p>
     </main>
   );
