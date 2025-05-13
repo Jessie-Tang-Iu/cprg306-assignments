fc38a0dd7b5dfe550ddf0da09a8d85e3765c77b0
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 09:34:22 -0600

    Update page.js
    modifying my home page

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import MyFirstComponent from "./_components/my-first-component";
 
 
 export default function Home() {
@@ -14,9 +15,9 @@ export default function Home() {
       {
         // test
       }
-      <h1 className="text-3xl">Hello World!</h1>
-      <p>Test123</p>
-
+      <h1 className="text-3xl">CPRG306 Class Examples</h1>
+      <p>Hello World!</p>
+      <MyFirstComponent />
       <h2>Course Example Links</h2>
       <ul>
         <li> <Link href="./week-2/" className="text-cyan-600 underline hover:text-cyan-300">Week 2 - Intro to React</Link> </li>
