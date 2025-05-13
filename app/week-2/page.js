a6225d83fee163127f96971749c36f1bb9e9f8d1
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 09:24:52 -0600

    Update page.js
    Get back to Home page

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 
 export default function WeekTwo() {
@@ -5,6 +6,8 @@ export default function WeekTwo() {
         <main>
             <h1>Second Page</h1>
             <p>This is the second page of my React app!</p>
+
+            <Link href="/">Home</Link>
         </main>
     );
 }
