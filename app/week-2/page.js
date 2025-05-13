a486681f7e1fb959cb32ea63695e6fbd4ebe7611
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 08:53:17 -0600

    Update page.js
    adding content

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -3,7 +3,8 @@
 export default function WeekTwo() {
     return (
         <main>
-            
+            <h1>Second Page</h1>
+            <p>This is the second page of my React app!</p>
         </main>
     );
 }
