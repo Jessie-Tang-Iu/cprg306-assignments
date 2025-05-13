8aa42331b1f230abca75882f604f8a96835069c6
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 12:36:29 -0600

    correctly the item.js and item-list.js accordingly

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,17 +1,11 @@
 
 
-export default function Item({props}) {
-
-    let item = { 
-        name: "", 
-        quantity: 0, 
-        category: "",
-    };
+export default function Item({name, quantity, category}) {
 
     return (
         <p className="px-4 py-2 m-8 bg-slate-800">
-            <p className="text-lg font-bold">{props.name}</p>
-            <p>Buy {props.quantity} in {props.category}</p>
+            <p className="text-lg font-bold">{name}</p>
+            <p>Buy {quantity} in {category}</p>
         </p>
     );
 }
