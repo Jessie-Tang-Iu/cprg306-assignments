b4cceb967d62d4b59f58689a5ff83da63dc98e31
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 09:34:16 -0600

    Create my-first-component.js
    Adding my first component

---
+++ app/_components/my-first-component.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function MyFirstComponent() {
+    return (
+        <div>
+            <h3>My First Component</h3>
+            <Link href="https://www.sait.ca">SAIT Website</Link>
+        </div>
+    );
+}
