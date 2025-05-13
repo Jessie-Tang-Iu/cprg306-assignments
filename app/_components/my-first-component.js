f51513b822af0c6d9e0807cb0cc50566a481d380
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-13 09:38:22 -0600

    Update my-first-component.js
    adding some math

--- app/_components/my-first-component.js
+++ app/_components/my-first-component.js
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 
 export default function MyFirstComponent() {
+
+    let a = 3;
+    let b = 4;
+
     return (
         <div>
             <h3>My First Component</h3>
             <Link href="https://www.sait.ca" className="text-cyan-600 underline hover:text-cyan-300">SAIT Website</Link>
+            <p>{a} added to {b} equals {a+b}</p>
         </div>
     );
 }
