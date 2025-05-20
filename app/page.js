02bd5302db86e5941ecb65e1874b7ed1eb442ee1
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-20 08:28:29 -0600

    Update with week 3 styles and create week 4 files

--- app/page.js
+++ app/page.js
@@ -2,16 +2,17 @@ import Link from "next/link";
 
 
 export default function Home() {
+
+  let linkStyles = "text-cyan-600 underline hover:text-cyan-300";
     return (
     <main>
       <h1 className="text-3xl"><strong>CPRG 306: Web Development 2 - Assignments</strong></h1>
       <br />
       <h2>Course Assignment Links : </h2>
       <ul>
-        <li> <Link href="./week-2/" className="text-cyan-600 underline hover:text-cyan-300">Week 2 - Link To My Week 2 Assignment</Link> </li>
-      </ul>
-      <ul>
-        <li> <Link href="./week-3/" className="text-cyan-600 underline hover:text-cyan-300">Week 3 - Build the Shopping List</Link> </li>
+        <li> <Link href="./week-2/" className={linkStyles}>Week 2 - Link To My Week 2 Assignment</Link> </li>
+        <li> <Link href="./week-3/objects" className={linkStyles}>Week 3 - objects</Link> </li>
+        <li> <Link href="./week-3/props" className={linkStyles}>Week 3 - props</Link> </li>
       </ul>
     </main>
   );
