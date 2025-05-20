5df49d98764e545e76ab159af5c7e828f388bc3e
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-20 10:07:10 -0600

    Update week3 props pages

---
+++ app/week-3/props/page.js
@@ -0,0 +1,22 @@
+import StduentComponent from "./student-component";
+
+export default function PropsPage(){
+    
+    let studentOne = {
+        studentName: "Joe",
+        studentAge: 24,
+        hasGraduated: false,
+        schedule: ["CPRG123","CPRG456","CPRG789","CPRG306"],
+        address: {
+            street: "123 Main St.",
+            city: "Calgary",
+            province: "AB"
+        }
+    }
+
+    return(
+        <main>
+            <StduentComponent sName={studentOne.studentName} sAge={studentOne.studentAge} sCity={studentOne.address.city} />
+        </main>
+    );
+}
---
+++ app/week-3/props/student-component.js
@@ -0,0 +1,11 @@
+
+
+export default function StduentComponent(props){
+    return(
+        <div>
+            <h3>{props.sName}</h3>
+            <p>Age: {props.sAge}</p>
+            <p></p>
+        </div>
+    );
+}
