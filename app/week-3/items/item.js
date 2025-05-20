2ec4e070e952471911bfea4c1b00c40df1e4b38d
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-20 17:25:51 -0600

    testing destructuring

--- app/week-3/items/item.js
+++ app/week-3/items/item.js
@@ -2,8 +2,11 @@
 
 export default function Item(item) {
 
+    let {name: iName} = item;
+
     return (
         <p className="px-4 py-2 m-8 bg-slate-800">
+            <p>{iName}</p>
             <p className="text-lg font-bold">{item.name}</p>
             <p>Buy {item.quantity} in {item.category}</p>
         </p>
