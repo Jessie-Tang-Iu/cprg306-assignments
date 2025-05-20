5bb4eec81cd148837393859722558200ef6efc1c
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-20 15:36:57 -0600

    Creating the link for assignment 3 and put it in items' folder

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ import Link from "next/link";
 export default function Home() {
 
   let linkStyles = "text-cyan-600 underline hover:text-cyan-300";
-    return (
+
+  return (
     <main>
       <h1 className="text-3xl"><strong>CPRG 306: Web Development 2 - Assignments</strong></h1>
       <br />
@@ -13,6 +14,7 @@ export default function Home() {
         <li> <Link href="./week-2/" className={linkStyles}>Week 2 - Link To My Week 2 Assignment</Link> </li>
         <li> <Link href="./week-3/objects" className={linkStyles}>Week 3 - objects</Link> </li>
         <li> <Link href="./week-3/props" className={linkStyles}>Week 3 - props</Link> </li>
+        <li> <Link href="./week-3/items" className={linkStyles}>Week 3 - Link To My Week 3 Assignment</Link> </li>
       </ul>
     </main>
   );
--- app/week-3/props/student-component.js
+++ app/week-3/props/student-component.js
@@ -1,11 +1,18 @@
 
 
 export default function StduentComponent(props){
+
+    if( props.sAge.isNan() ){
+        let age = 0;
+    } else {
+        let age = props.sAge;
+    }
+
     return(
         <div>
             <h3>{props.sName}</h3>
             <p>Age: {props.sAge}</p>
-            <p></p>
+            <p>City: {props.sCity}</p>
         </div>
     );
 }
