91efa375f2bf3d1815851834dd8035fe39eadcea
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-20 16:14:48 -0600

    Update page.js and student-component.js

--- app/week-3/props/page.js
+++ app/week-3/props/page.js
@@ -1,4 +1,5 @@
 import StudentComponent from "./student-component";
+import StudentComponentNew from "./student-component-new";
 
 export default function PropsPage(){
     
@@ -14,9 +15,25 @@ export default function PropsPage(){
         }
     }
 
+    let studentTwo = {
+        studentName: "Frank",
+        studentAge: 23,
+        address: {
+            city: "Edmonton"
+        }
+    }
+
     return(
         <main>
-            <StudentComponent sName={studentOne.studentName} sAge={studentOne.studentAge} sCity={studentOne.address.city} />
+            <StudentComponent sName="Kevin" sAge="21" sCity="Edmonton" />
+            <StudentComponent sName="Bob" sAge="26" sCity="Calgary" />
+            <StudentComponent
+                sName={studentOne.studentName} 
+                sAge={studentOne.studentAge} 
+                sCity={studentOne.address.city} 
+            />
+            <StudentComponentNew studentObj={studentOne} />
+            <StudentComponentNew studentObj={studentTwo} />
         </main>
     );
 }
