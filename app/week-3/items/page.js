db9f1102790c18db9ba966064c665e1b0259fdae
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-20 15:43:51 -0600

    adding return to home page link

--- app/week-3/items/page.js
+++ app/week-3/items/page.js
@@ -6,6 +6,7 @@ export default function Page() {
         <main>
             <h1 className="text-3xl font-bold">Shopping List</h1>
             <ItemList />
+            <Link href="/" className="text-cyan-600 underline hover:text-cyan-300">Home</Link>
         </main>
     );
 }
