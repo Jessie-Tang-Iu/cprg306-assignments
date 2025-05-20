81c4646aa9e1e8c053a8ffc95cf84d3669274215
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-20 17:22:59 -0600

    Update item.js

--- app/week-3/items/item.js
+++ app/week-3/items/item.js
@@ -2,15 +2,10 @@
 
 export default function Item(item) {
 
-    let {name: itemName, 
-        quantity: itemQuantity, 
-        category: itemCategory
-    } = item;
-
     return (
         <p className="px-4 py-2 m-8 bg-slate-800">
-            <p className="text-lg font-bold">{itemName}</p>
-            <p>Buy {itemQuantity} in {itemCategory}</p>
+            <p className="text-lg font-bold">{item.name}</p>
+            <p>Buy {item.quantity} in {item.category}</p>
         </p>
     );
 }
