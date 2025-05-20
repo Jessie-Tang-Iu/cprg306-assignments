734be080e11ade0934931923eb3e32957df542fe
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-20 08:38:10 -0600

    Creating object and display information from the object

---
+++ app/week-3/objects/page.js
@@ -0,0 +1,26 @@
+
+
+
+export default function ObjectPage(){
+
+    let studentOne = {
+        studentName: "Joe",
+        studentAge: 24,
+        hasGraduated: false,
+        schedule: ["CPRG123","CPRG456","CPRG789","CPRG306"],
+        address: {
+            street: "123 Main St.",
+            city: "Calgary",
+            province: "AB"
+        }
+    }
+
+    return(
+        <main>
+            <h1>Objects</h1>
+            <h2>Dot Notation</h2>
+            <p>Name: {studentOne.studentName}</p>
+            <p></p>
+        </main>
+    );
+}
