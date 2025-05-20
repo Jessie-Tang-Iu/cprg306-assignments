ff5f6d9a249c6b2130a3e4733a842ddad93a6cd2
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-20 15:53:37 -0600

    using destructuring method

--- app/week-3/items/item.js
+++ app/week-3/items/item.js
@@ -1,7 +1,9 @@
 
 
-export default function Item({name, quantity, category}) {
+export default function Item(item) {
 
+    let {name, quantity, category} = item;
+    
     return (
         <p className="px-4 py-2 m-8 bg-slate-800">
             <p className="text-lg font-bold">{name}</p>
