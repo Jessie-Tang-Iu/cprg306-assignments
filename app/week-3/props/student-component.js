fdf550894c2b61e434b766a20db66c17f58ee594
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-20 16:10:23 -0600

    creating student-component-new, modifying student-component and page.js

--- app/week-3/props/student-component.js
+++ app/week-3/props/student-component.js
@@ -3,7 +3,7 @@
 export default function StudentComponent(props){
 
     return(
-        <div>
+        <div className="big green-400 m-5 p-3 border-2 border-bblue-500 w-40 rounded">
             <h3>{props.sName}</h3>
             <p>Age: {props.sAge}</p>
             <p>City: {props.sCity}</p>
